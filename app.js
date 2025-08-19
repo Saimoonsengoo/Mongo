49cac36a41a18a33b4047cf092498d2711e47343
Steven <155521769+Saimoonsengoo@users.noreply.github.com>
2025-08-19 20:03:54 +0630

    'Express2'

--- app.js
+++ app.js
@@ -2,30 +2,41 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
+//using view engine(ejs)
+app.set('views', './views');
+app.set('view engine', 'ejs')
+
+let blogs = [
+    {title: "Blog 1", into: "This is blof 1 introduction."},
+    {title: "Blog 2", into: "This is blof 2 introduction."},
+    {title: "Blog 3", into: "This is blof 3 introduction."}
+]
+
 app.get('/',(req,res)=>{
-    res.sendFile('./views/home.html', {root : __dirname})
+    //dont need to use sendFile
+    // res.sendFile('./views/home.html', {root : __dirname})
+    res.render('home', {
+        //sending data
+        blogs
+    })
 })
 
 app.get('/about',(req,res)=>{
-    res.sendFile('./views/about.html', {root : __dirname})
-})
-
-app.get('/about-us',(req,res)=>{
-    res.redirect('/about')
+    // res.sendFile('./views/about.html', {root : __dirname})
+     res.render('about')
 })
 
 app.get('/contact',(req,res)=>{
-    res.sendFile('./views/contact.html', {root : __dirname})
-})
-
-app.get('/contact-us',(req,res)=>{
-    res.redirect('/contact')
+    // res.sendFile('./views/contact.html', {root : __dirname})
+     res.render('contact')
 })
 
 // use (use function) to handle 404 Page
 app.use((req,res)=>{
     res.status(404);
-    res.sendFile('./views/404.html', {root: __dirname})
+    res.render('404');
+    //res.status(404).render('404');
+    // res.sendFile('./views/404.html', {root: __dirname})
 })
 
 //remark code is runing line by line if 404 direct function is append on the about-us => about-us dont run anymore
